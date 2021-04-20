cfe375668341a918a3491cce9cb4ed124de062d4
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-04-20 12:01:51 +0100

    FP controller working

---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+
+    [SerializeField]
+    public float walkSpeed = 3.0f;
+    
+    [SerializeField]
+    public float runSpeed = 6.0f;
+
+    [SerializeField]
+    public float smoothMoveTime = 0.1f; //smoothing
+
+    [SerializeField]
+    public float jumpHeight = 8.0f;
+
+    [SerializeField]
+    public float gravity = 18.0f;
+
+    [SerializeField]
+    public bool lockCursor;
+
+    [SerializeField]
+    public float mouseSensitivity = 10.0f;
+
+    [SerializeField]
+    public Vector2 pitchMinMax = new Vector2(-40.0f, 85.0f);
+
+    [SerializeField]
+    public float rotationSmoothTime = 0.1f;
+
+    CharacterController controller;
+    Camera cam;
+
+    [SerializeField]
+    public float yaw;
+
+    [SerializeField]
+    public float pitch;
+
+    private float smoothYaw;
+    private float smoothPitch;
+
+    private float yawSmoothV;
+    private float pitchSmoothV;
+    private float verticalVelocity;
+
+    private Vector3 velocity;
+    private Vector3 smoothV;
+    private Vector3 rotationSmoothVelocity;
+    private Vector3 currentRotation;
+
+    private bool jumping;
+    private float lastGrounedTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        controller = GetComponent<CharacterController>();
+
+        yaw = transform.eulerAngles.y;
+        pitch = cam.transform.localEulerAngles.x; //local = relative to parent's position
+        smoothYaw = yaw;
+        smoothPitch = pitch;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        MouseInput();
+    }
+
+
+    private void Movement()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Vector3 inputDir = new Vector3(input.x, 0, input.y).normalized;
+        Vector3 worldInputDir = transform.TransformDirection(inputDir); //local to world space
+
+        float currentSpeed = (Input.GetKey(KeyCode.LeftShift)) ? runSpeed : walkSpeed;
+        Vector3 targetVelocity = worldInputDir * currentSpeed;
+        velocity = Vector3.SmoothDamp(velocity, targetVelocity, ref smoothV, smoothMoveTime);
+
+        verticalVelocity -= gravity * Time.deltaTime;
+        velocity = new Vector3(velocity.x, verticalVelocity, velocity.z);
+
+        var flags = controller.Move(velocity * Time.deltaTime);
+        if (flags == CollisionFlags.Below)
+        {
+            jumping = false;
+            lastGrounedTime = Time.time;
+            verticalVelocity = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float timerSinceGrounded = Time.time - lastGrounedTime;
+            if (controller.isGrounded || (!jumping && timerSinceGrounded < 0.15f))
+            {
+                jumping = true;
+                verticalVelocity = jumpHeight;
+            }
+        }
+    }
+
+    private void MouseInput()
+    {
+        float mX = Input.GetAxisRaw("Mouse X");
+        float mY = Input.GetAxisRaw("Mouse Y");
+
+        //gross hack to stop camera swinging down at start
+        float mMag = Mathf.Sqrt(mX * mX + mY * mY);
+        if (mMag > 5)
+        {
+            mX = 0;
+            mY = 0;
+        }
+
+        yaw += mX * mouseSensitivity;
+        pitch -= mY * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        smoothPitch = Mathf.SmoothDampAngle(smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
+        smoothYaw = Mathf.SmoothDampAngle(smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
+
+        transform.eulerAngles = Vector3.up * smoothYaw;
+        cam.transform.localEulerAngles = Vector3.right * smoothPitch;
+    }
+}
