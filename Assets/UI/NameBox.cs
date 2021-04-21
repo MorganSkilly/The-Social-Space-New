23abf60c79ee09de519907820c9133ce7d8ed117
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-04-21 14:37:27 +0100

    Screen 1 of Welcome UI

---
+++ Assets/UI/NameBox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameBox : MonoBehaviour
+{
+    public Button button;
+    public InputField inputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.interactable = false;
+    }
+
+    public void inputChange()
+    {
+        if(inputField.text != null)
+        {
+            button.interactable = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UI/WelcomeUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WelcomeUI : MonoBehaviour
+{
+    public GameObject Screen1;
+    public GameObject Screen2;
+    public GameObject Transition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen2.SetActive(false);
+        Transition.SetActive(false);
+    }
+
+    public void DisplayNamePicked()
+    {
+        StartCoroutine(startTransition());
+    }
+
+    IEnumerator startTransition()
+    {
+        Transition.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        Screen1.SetActive(false);
+        Screen2.SetActive(true);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
