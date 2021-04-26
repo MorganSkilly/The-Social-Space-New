1dce1a2625fd561686dbce7cdb0d2470123e3eda
Morgan Skillicorn <hello@morgan.games>
2021-04-26 11:46:45 +0100

    added AV pro player

---
+++ Assets/mediaPlayerInteraction.cs
@@ -0,0 +1,20 @@
+﻿using RenderHeads.Media.AVProVideo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mediaPlayerInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<MediaPlayer>().AudioSource.transform.position = gameObject.transform.position;
+        gameObject.GetComponent<MediaPlayer>().AudioSource.spatialBlend = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
